662dae585fa9c89ba68db9871541184a55d0659b
elizabethnewman <elizabeth.newman@emory.edu>
2022-10-18 16:50:03 -0400

    no message

--- tnn/layers/__init__.py
+++ tnn/layers/__init__.py
@@ -1,6 +1,7 @@
-from .single_layers import LinearLayer, AntiSymmetricLayer, HamiltonianLayer
+from .single_layers import LinearLayer, AntiSymmetricLayer, HamiltonianLayer, ModeKLayer
 from .resnet_layers import ResidualLayer, AntiSymmetricResidualLayer, HamiltonianResidualLayer
 from .t_single_layers import tLinearLayer, tAntiSymmetricLayer, tHamiltonianLayer
 from .t_resnet_layers import tResidualLayer, tAntiSymmetricResidualLayer, tHamiltonianResidualLayer
-from .reshape import View, Permute
+from .reshape import View, Permute, Unfold
+
 
