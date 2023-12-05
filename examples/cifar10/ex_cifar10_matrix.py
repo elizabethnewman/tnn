2cdbccd9004ee47010eace461b464202f2d00656
elizabethnewman <39045034+elizabethnewman@users.noreply.github.com>
2023-12-05 13:10:31 -0500

    new saving protocol

--- examples/cifar10/ex_cifar10_matrix.py
+++ examples/cifar10/ex_cifar10_matrix.py
@@ -87,7 +87,7 @@ logger.info("--------------------------------------------------\n")
 # train!
 t0 = time.perf_counter()
 results = train(net, loss, optimizer, train_loader, val_loader, test_loader, max_epochs=args.max_epochs,
-                scheduler=scheduler, regularizer=regularizer, device=device, logger=logger)
+                scheduler=scheduler, regularizer=regularizer, device=device, logger=logger, sPath=sPath)
 t1 = time.perf_counter()
 
 
